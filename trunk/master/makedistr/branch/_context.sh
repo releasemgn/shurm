6458c192230a8a6ae18314a2e85485fda13d2c01
vsavchik <vsavchik@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2013-12-04 16:35:20 +0000

    add last tag

--- trunk/master/makedistr/branch/_context.sh
+++ trunk/master/makedistr/branch/_context.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export LAST_PROD_TAG=`cat last-prod-tag.txt`
-
 C_CONTEXT_VERSIONMODE=branch
--- trunk/master/makedistr/branch/codebase-dropcandidatetags.sh
+++ trunk/master/makedistr/branch/codebase-dropcandidatetags.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
 export VERSION_MODE=branch
-export LAST_PROD_TAG=`cat last-prod-tag.txt`
 export OUTDIR=branch
 
 cd ..
