b4152aa427842bbce2db3d8bca91a9271be16c26
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-02-20 15:39:37 +0000

    release 1.30

---
+++ tags/release-1.30/makedistr/majorbranch/getall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+cd ..
+. ./getopts.sh
+
+DOWNLOAD_PROJECT=$1
+if [ "$DOWNLOAD_PROJECT" != "" ]; then
+	shift 1
+	DOWNLOAD_PROJECT_ITEMS=$*
+else
+	DOWNLOAD_PROJECT_ITEMS=
+fi
+
+# execute
+
+export VERSION_MODE=majorbranch
+export OUTDIR=$VERSION_MODE
+
+. ./common.sh
+
+TAG_VERSION=$C_CONFIG_NEXT_MAJORRELEASE
+TAG_GETALL=$C_CONFIG_PRODMAJOR_TAG
+
+./getall.sh $TAG_VERSION $TAG_GETALL $DOWNLOAD_PROJECT "$DOWNLOAD_PROJECT_ITEMS"
+
+cd $VERSION_MODE
+
+echo getall.sh finished.
