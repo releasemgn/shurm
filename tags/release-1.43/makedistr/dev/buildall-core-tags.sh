298a0076d86016455ea8b453c91142dc28e5a62d
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-03-12 08:37:55 +0000

    release 1.43

---
+++ tags/release-1.43/makedistr/dev/buildall-core-tags.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+cd `dirname $0`
+RUNDIR=`pwd`
+
+. ../getopts.sh
+
+TAG=$1
+
+# check params
+if [ "$TAG" = "" ]; then
+	echo TAG not set
+	exit 1
+fi
+shift 1
+
+MODULEPROJECTS=$*
+
+# execute
+. ./_context.sh
+export VERSION_MODE=$C_CONTEXT_VERSIONMODE
+
+cd ..
+
+export OUTDIR=$VERSION_MODE/tag-$TAG
+mkdir -p $OUTDIR
+
+TSVALUE=`date +%Y-%m-%d.%H-%M-%S`
+./buildall-core-tags.sh $TAG $OUTDIR $MODULEPROJECTS > $OUTDIR/buildall-core-tags-$TSVALUE.out 2>&1
+
+cd $VERSION_MODE
+
+echo buildall-core-tags.sh: finished
