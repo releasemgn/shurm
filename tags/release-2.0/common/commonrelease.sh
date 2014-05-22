6b8477ffffd152a91621496d68dcc47b4350b6a7
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-05-22 16:08:27 +0000

    release 2.0

---
+++ tags/release-2.0/common/commonrelease.sh
@@ -0,0 +1,325 @@
+#!/bin/bash
+# Copyright 2011-2013 vsavchik@gmail.com
+
+C_RELEASE_FNAME=
+C_RELEASE_XMLVALUE=
+
+# C_RELEASE_PROPERTY_OBSOLETE=
+
+C_RELEASE_TARGETS=
+C_RELEASE_ITEMS=
+C_RELEASE_ALLITEMS=
+C_RELEASE_CONFCOMPLIST=
+C_RELEASE_CONFCOMPPATH=
+C_RELEASE_CONFCOMP_PARTIAL=
+
+C_RELEASE_PROJECT_TAG=
+C_RELEASE_PROJECT_BRANCH=
+C_RELEASE_PROJECT_VERSION=
+
+C_RELEASE_DISTRID=
+C_RELEASE_SRCDIR=
+C_RELEASE_SRCVER=
+
+C_RELEASE_CMD_RES=
+C_RELEASE_FINDFILE_NAME=
+
+function f_release_getxmlproperty() {
+	local P_PROPNAME=$1
+	C_RELEASE_XMLVALUE=`xmlstarlet sel -t -m "release/property[@name='$P_PROPNAME']" -v "@value" $C_RELEASE_FNAME`
+}
+
+function f_release_setfile() {
+	local P_FNAME=$1
+
+	if [ ! -f "$P_FNAME" ]; then
+		echo "f_release_setfile: unable to find release definition file $P_FNAME. Exiting"
+		exit 1
+	fi
+
+	C_RELEASE_FNAME=$P_FNAME
+
+	# release properties
+	f_release_getxmlproperty obsolete
+	C_RELEASE_PROPERTY_OBSOLETE=$C_RELEASE_XMLVALUE
+}
+
+function f_release_getprojectinfo() {
+	local P_PROJECTSET=$1
+	local P_PROJECT=$2
+
+	C_RELEASE_PROJECT_TAG=
+	C_RELEASE_PROJECT_BRANCH=
+	C_RELEASE_PROJECT_VERSION=
+
+	# check all case
+	local F_CHECKALL=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']" -v "@all" $C_RELEASE_FNAME`
+	if [ "$F_CHECKALL" = "true" ]; then
+		C_RELEASE_PROJECT_TAG=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']" -v "@buildtag" $C_RELEASE_FNAME`
+	else	
+		C_RELEASE_PROJECT_TAG=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project[@name='$P_PROJECT']" -v "@buildtag" $C_RELEASE_FNAME`
+	fi
+
+	C_RELEASE_PROJECT_BRANCH=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project[@name='$P_PROJECT']" -v "@buildbranch" $C_RELEASE_FNAME`
+	C_RELEASE_PROJECT_VERSION=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project[@name='$P_PROJECT']" -v "@buildversion" $C_RELEASE_FNAME`
+}
+
+function f_release_getprojects() {
+	local P_PROJECTSET=$1
+
+	# check all case
+	local F_CHECKALL=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']" -v "@all" $C_RELEASE_FNAME`
+	if [ "$F_CHECKALL" = "true" ]; then
+		C_RELEASE_TARGETS="all"
+	else	
+		C_RELEASE_TARGETS=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project" -v "@name" -o " " $C_RELEASE_FNAME`
+		C_RELEASE_TARGETS=${C_RELEASE_TARGETS% }
+		C_RELEASE_TARGETS=${C_RELEASE_TARGETS# }
+	fi
+}
+
+function f_release_getprojectitems() {
+	local P_PROJECTSET=$1
+	local P_PROJECTNAME=$2
+
+	C_RELEASE_ALLITEMS=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project[@name='$P_PROJECTNAME']" -v "@all" $C_RELEASE_FNAME`
+	if [ "$C_RELEASE_ALLITEMS" = "" ]; then
+		C_RELEASE_ALLITEMS="true"
+	fi
+
+	C_RELEASE_ITEMS=`xmlstarlet sel -t -m "release/build/buildset[@type='$P_PROJECTSET']/project[@name='$P_PROJECTNAME']/distitem" -v "@name" -o " " $C_RELEASE_FNAME`
+	C_RELEASE_ITEMS=${C_RELEASE_ITEMS% }
+	C_RELEASE_ITEMS=${C_RELEASE_ITEMS# }
+}
+
+function f_release_getconfcomplist() {
+	# check all case
+	local F_CHECKALL=`xmlstarlet sel -t -m "release/configure" -v "@all" $C_RELEASE_FNAME`
+	if [ "$F_CHECKALL" = "true" ]; then
+		C_RELEASE_CONFCOMPLIST="all"
+	else
+		C_RELEASE_CONFCOMPLIST=`xmlstarlet sel -t -m "release/configure/component" -v "@name" -o " " $C_RELEASE_FNAME`
+		C_RELEASE_CONFCOMPLIST=${C_RELEASE_CONFCOMPLIST% }
+		C_RELEASE_CONFCOMPLIST=${C_RELEASE_CONFCOMPLIST# }
+	fi
+}
+
+function f_release_getconfcomppath() {
+	local P_DC=$1
+	local P_SERVER=$2
+	local P_HOSTNAME=$3
+	local P_CONFCOMP=$4
+	local P_CONFCOMPLAYER=$5
+
+	if [ "$P_CONFCOMPLAYER" = "env" ]; then
+		C_RELEASE_CONFCOMPPATH=common/$P_CONFCOMP
+	elif [ "$P_CONFCOMPLAYER" = "datacenter" ]; then
+		C_RELEASE_CONFCOMPPATH=$P_DC/common/$P_CONFCOMP
+	elif [ "$P_CONFCOMPLAYER" = "server" ]; then
+		C_RELEASE_CONFCOMPPATH=$P_DC/$P_SERVER/$P_CONFCOMP
+	elif [ "$P_CONFCOMPLAYER" = "node" ]; then
+		C_RELEASE_CONFCOMPPATH=$P_DC/$P_SERVER/$P_CONFCOMP@$P_HOSTNAME
+	else
+		echo f_release_getconfcomppath: invalid configuration component layer=$P_CONFCOMPLAYER. Exiting
+		exit 1
+	fi
+}
+
+function f_release_getconfcompinfo() {
+	local P_CONFCOMP=$1
+	C_RELEASE_CONFCOMP_PARTIAL=`xmlstarlet sel -t -m "release/configure/component[@name='$P_CONFCOMP']" -v "@partial" $C_RELEASE_FNAME`
+}
+
+# run any command
+function f_release_runcmd() {
+	local P_CMD="$1"
+
+	C_RELEASE_CMD_RES=
+	if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+		C_RELEASE_CMD_RES=`(eval $P_CMD) 2>&1`
+		if [ $? -ne 0 ]; then
+			return 1
+		fi
+	else
+		if [ "$C_ENV_PROPERTY_DISTR_REMOTEHOST" = "" ]; then
+			echo "f_release_runcmd: C_ENV_PROPERTY_DISTR_REMOTEHOST is not set. Exiting"
+			exit 1
+		fi
+
+		if [ "$C_ENV_PROPERTY_KEYNAME" != "" ]; then
+			C_RELEASE_CMD_RES=`ssh -i $C_ENV_PROPERTY_KEYNAME -n $C_ENV_PROPERTY_DISTR_REMOTEHOST "$P_CMD" 2>&1`
+			if [ $? -ne 0 ]; then
+				return 1
+			fi
+		else
+			C_RELEASE_CMD_RES=`ssh -n $C_ENV_PROPERTY_DISTR_REMOTEHOST "$P_CMD" 2>&1`
+			if [ $? -ne 0 ]; then
+				return 1
+			fi
+		fi
+	fi
+	return 0
+}
+
+function f_release_runcmdcheck() {
+	local P_CMD="$1"
+
+	f_release_runcmd "$P_CMD"
+	if [ $? -ne 0 ]; then
+		if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+			echo "f_release_runcmdcheck: unable to execute $P_CMD. Exiting"
+		else
+			echo "f_release_runcmdcheck: unable to execute $P_CMD on $C_ENV_PROPERTY_DISTR_REMOTEHOST. Exiting"
+		fi
+		exit 1
+	fi
+}
+
+function f_release_getfullproddistr() {
+	C_RELEASE_DISTRID=
+
+	f_release_runcmdcheck "find $C_ENV_PROPERTY_DISTR_PATH -maxdepth 1 -name \"*-prod\" -exec basename {} \\;"
+	local F_NAME=$C_RELEASE_CMD_RES
+
+	# check content
+	local F_WORDS=`echo $F_NAME | wc -w`
+	if [ "$F_WORDS" = "0" ]; then
+		echo "f_release_getfullproddistr: unable to find prod distributive. Exiting"
+		exit 1
+	fi
+
+	if [ "$F_WORDS" != "1" ]; then
+		echo "f_release_getfullproddistr: ambiguus distributives - $F_NAME. Exiting"
+		exit 1
+	fi
+
+	C_RELEASE_DISTRID=$F_NAME
+}
+
+function f_release_resolverelease() {
+	P_RELEASE=$1
+
+	C_RELEASE_DISTRID=
+	if [ "$P_RELEASE" = "prod" ]; then
+		f_release_getfullproddistr
+	elif [ "$P_RELEASE" = "last" ]; then
+		C_RELEASE_DISTRID=$C_CONFIG_VERSION_LAST_FULL
+	elif [ "$P_RELEASE" = "next" ]; then
+		C_RELEASE_DISTRID=$C_CONFIG_VERSION_NEXT_FULL
+	else
+		C_RELEASE_DISTRID=$P_RELEASE
+	fi
+}
+
+function f_release_getdistrdir() {
+	local P_RELEASENAME=$1
+
+	C_RELEASE_SRCDIR=$C_ENV_PROPERTY_DISTR_PATH/$P_RELEASENAME
+	C_RELEASE_SRCVER=`basename $C_RELEASE_SRCDIR | cut -d "-" -f1`
+	if [ "$C_RELEASE_SRCVER" = "" ]; then
+		echo "f_release_getdistrdir: SRCDIR is expected having name=VERSION-anything, value=$C_RELEASE_SRCDIR. Exiting"
+		exit 1
+	fi
+
+	echo check source dir $C_RELEASE_SRCDIR...
+	f_release_runcmdcheck "if [ -d "$C_RELEASE_SRCDIR" ]; then echo true; fi"
+	if [ "$C_RELEASE_CMD_RES" != "true" ]; then
+		if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+			echo "f_release_getdistrdir: local release directory $C_RELEASE_SRCDIR does not exist. Exiting"
+		else
+			echo "f_release_getdistrdir: $P_DISTR_HOSTLOGIN - release directory $C_RELEASE_SRCDIR does not exist. Exiting"
+		fi
+		exit 1
+	fi
+
+	if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+		echo "local source dir found path=$C_RELEASE_SRCDIR, version=$C_RELEASE_SRCVER."
+	else
+		echo "$P_DISTR_HOSTLOGIN: source dir found path=$C_RELEASE_SRCDIR, version=$C_RELEASE_SRCVER."
+	fi
+}
+
+function f_release_downloadfile() {
+	local P_SRCPATH=$1
+	local P_DSTPATH=$2
+
+	if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+		if [ ! -f "$P_SRCPATH" ]; then
+			echo "f_release_downloadfile: file is not found - $P_SRCPATH. Exiting"
+			exit 1
+		fi
+
+		cp -p $P_SRCPATH $P_DSTPATH
+		if [ $? -ne 0 ]; then
+			return 1
+		fi
+
+	else
+		if [ "$C_ENV_PROPERTY_DISTR_REMOTEHOST" = "" ]; then
+			echo "f_release_downloadfile: C_ENV_PROPERTY_DISTR_REMOTEHOST is not set. Exiting"
+			exit 1
+		fi
+
+		# get remote file - using key file if any
+		if [ "$C_ENV_PROPERTY_KEYNAME" != "" ]; then
+			scp -q -B -p -i $C_ENV_PROPERTY_KEYNAME $C_ENV_PROPERTY_DISTR_REMOTEHOST:$P_SRCPATH $P_DSTPATH
+			if [ $? -ne 0 ]; then
+				return 1
+			fi
+		else
+			scp -q -B -p $C_ENV_PROPERTY_DISTR_REMOTEHOST:$P_SRCPATH $P_DSTPATH
+			if [ $? -ne 0 ]; then
+				return 1
+			fi
+		fi
+	fi
+
+	return 0
+}
+
+function f_release_downloaddir() {
+	local P_SRCPATH=$1
+	local P_DSTPATH=$2
+
+	if [ "$C_ENV_PROPERTY_DISTR_USELOCAL" = "true" ]; then
+		if [ -d "$P_SRCPATH" ]; then
+			cp -R $P_SRCPATH $P_DSTPATH
+			if [ $? -ne 0 ]; then
+				return 1
+			fi
+		fi
+	else
+		f_release_runcmdcheck "if [ -d $P_SRCPATH ]; then echo ok; fi"
+		if [ "$C_RELEASE_CMD_RES" = "ok" ]; then
+			if [ "$C_ENV_PROPERTY_KEYNAME" != "" ]; then
+				scp -q -r -B -p -i $C_ENV_PROPERTY_KEYNAME $C_ENV_PROPERTY_DISTR_REMOTEHOST:$P_SRCPATH $P_DSTPATH
+				if [ $? -ne 0 ]; then
+					return 1
+				fi
+			else
+				scp -q -r -B -p $C_ENV_PROPERTY_DISTR_REMOTEHOST:$P_SRCPATH $P_DSTPATH
+				if [ $? -ne 0 ]; then
+					return 1
+				fi
+			fi
+		fi
+	fi
+
+	return 0
+}
+
+function f_release_findfile() {
+	local P_SRCDIR=$1
+	local P_XBASENAME=$2
+	local P_XEXTENTION=$3
+
+	f_release_runcmd "if [ -d "$P_SRCDIR" ]; then cd $P_SRCDIR; find . -maxdepth 1 -type f -name \"*$P_XEXTENTION\" | egrep \"./$P_XBASENAME$P_XEXTENTION|./.*[0-9]-$P_XBASENAME$P_XEXTENTION|./$P_XBASENAME-[0-9].*$P_XEXTENTION\"; fi"
+	C_RELEASE_FINDFILE_NAME=$C_RELEASE_CMD_RES
+
+	local F_LOCAL_COUNT=`echo "$C_RELEASE_FINDFILE_NAME" | wc -l`
+	if [ "$F_LOCAL_COUNT" != "1" ]; then
+		local F_SHOWNAMES=`echo $C_RELEASE_FINDFILE_NAME | tr "\n" " "`
+		echo "f_find_file: too many files ($F_SHOWNAMES) with $P_XBASENAME$P_XEXTENTION exist in $P_SRCDIR. Exiting."
+		exit 1
+	fi
+}
---
+++ tags/release-2.0/deployment/configure.sh
@@ -0,0 +1,346 @@
+#!/bin/bash
+# Copyright 2011-2013 vsavchik@gmail.com
+
+cd `dirname $0`
+. ./getopts.sh
+. ./common.sh
+
+# check params
+# should be specific DC
+DC=$GETOPT_DC
+if [ "$DC" = "" ]; then
+	echo configure.sh: DC not set
+	exit
+fi
+
+P_DIR_TYPE=$1
+P_DIR_TEMPLATES=$2
+P_DIR_LIVE=$3
+
+if [ "$P_DIR_TYPE" = "" ]; then
+	echo configure.sh: P_DIR_TYPE not set
+	exit 1
+fi
+if [ "$P_DIR_TEMPLATES" = "" ]; then
+	echo configure.sh: P_DIR_TEMPLATES not set
+	exit 1
+fi
+if [ "$P_DIR_LIVE" = "" ]; then
+	echo configure.sh: P_DIR_LIVE not set
+	exit 1
+fi
+shift 3
+
+SRVNAME_LIST=$*
+
+# execute
+if [ "$C_ENV_ID" = "" ]; then
+	echo configure.sh: C_ENV_ID not set
+	exit 1
+fi
+
+S_CONFIGURE_TMP="/tmp/$HOSTNAME.$USER.configure.p$$"
+S_CONFIGURE_AWKPROGRAM="$S_CONFIGURE_TMP/process.awk"
+S_CONFIGURE_PREPAREDTEMPLATES="$S_CONFIGURE_TMP/templates.prepared"
+
+S_CONFIGURE_PROPLIST_SECRET=
+S_CONFIGURE_PROPLIST_ENV=
+S_CONFIGURE_PROPLIST_DC=
+S_CONFIGURE_PROPLIST_SERVER=
+
+function f_local_generatenodecomponentfiles() {
+	local P_SERVER=$1
+	local P_NODEHOSTLOGIN=$2
+	local P_CONFCOMPNAME=$3
+
+	# check dir type
+	local F_COMPSUBDIR=$P_CONFCOMPNAME
+	if [ "$P_DIR_TYPE" = "templates" ]; then
+		f_distr_getconfcompinfo $P_CONFCOMPNAME
+		if [ "$C_DISTR_CONF_SUBDIR" != "" ]; then
+			F_COMPSUBDIR=$C_DISTR_CONF_SUBDIR/$P_CONFCOMPNAME
+		fi
+	fi
+
+	# check component is defined in templates
+	if [ ! -d $S_CONFIGURE_PREPAREDTEMPLATES/$F_COMPSUBDIR ]; then
+		if [ "$GETOPT_DEPLOYPARTIALCONF" != "yes" ]; then
+			echo f_local_generatenodecomponentfiles: $P_CONFCOMPNAME component is not found in $S_CONFIGURE_PREPAREDTEMPLATES. Exiting
+			exit 1
+		fi
+		return 1
+	fi
+
+	# process component files
+	local F_LOCAL_DIR_FROM=$S_CONFIGURE_PREPAREDTEMPLATES/$F_COMPSUBDIR
+	local F_LOCAL_DIR_TO=$P_DIR_LIVE/$DC/$P_SERVER/$P_NODEHOSTLOGIN/$P_CONFCOMPNAME
+
+	echo generate component=$P_CONFCOMPNAME to $F_LOCAL_DIR_TO ...
+
+	rm -rf $F_LOCAL_DIR_TO
+	mkdir -p $F_LOCAL_DIR_TO
+
+	local F_COMPSAVEDIR=`pwd`
+	cd $F_LOCAL_DIR_FROM
+
+	local fname
+	local fname_dst
+	for fname in `find . -type d`; do
+		fname_dst=$F_LOCAL_DIR_TO/$fname
+		mkdir -p $fname_dst
+	done
+
+	for fname in `find . -type f`; do
+		fname_dst=$F_LOCAL_DIR_TO/$fname
+		mkdir -p `dirname $fname_dst`
+
+		local F_BASENAME=`basename $fname`
+		local F_EXT=${F_BASENAME##*.}
+
+		if [[ " $S_COMMON_EXTLIST " =~ " $F_EXT " ]]; then
+			# copy and process parameters
+			awk -f $S_CONFIGURE_AWKPROGRAM $fname > $fname_dst
+		else
+			# copy binary as is
+			cp $fname $fname_dst
+		fi
+
+	done
+
+	cd $F_COMPSAVEDIR
+}
+
+function f_local_generatenodefiles() {
+	local P_SERVER=$1
+	local P_NODEHOSTLOGIN=$2
+	local P_CONFCOMPLIST="$3"
+
+	if [ "$GETOPT_SHOWALL" = "yes" ]; then
+		echo "configure node=$P_NODEHOSTLOGIN, components=($P_CONFCOMPLIST)..."
+	fi
+
+	local F_LOCAL_SAVEPATH=`pwd`
+	cd $S_CONFIGURE_PREPAREDTEMPLATES
+
+	# generate template files
+	local comp
+	for comp in $P_CONFCOMPLIST; do
+		f_local_generatenodecomponentfiles $P_SERVER $P_NODEHOSTLOGIN $comp
+	done
+
+	cd $F_LOCAL_SAVEPATH
+}
+
+function f_local_construct_addawkvar() {
+	local P_VAR=$1
+	local P_VALUE=$2
+
+	local awk_var=`echo $P_VAR | tr ".-" "__"`
+	P_VALUE=${P_VALUE/&/\\\\&}
+
+	echo "$awk_var=\"$P_VALUE\";" >> $S_CONFIGURE_AWKPROGRAM
+	echo "gsub(/@$P_VAR@/, $awk_var);" >> $S_CONFIGURE_AWKPROGRAM
+}
+
+function f_local_construct_awk() {
+	local P_SERVER=$1
+
+	S_CONFIGURE_AWK_VARS=
+	echo "{" > $S_CONFIGURE_AWKPROGRAM
+
+	# add server-level props
+	local var
+	for var in $S_CONFIGURE_PROPLIST_SERVER; do
+		f_env_getserverpropertyvalue $DC $P_SERVER $var
+		f_local_construct_addawkvar server.$var "$C_ENV_XMLVALUE"
+
+		# handle unprefixed variables
+		f_local_construct_addawkvar $var "$C_ENV_XMLVALUE"
+	done
+
+	# add dc-level props
+	for var in $S_CONFIGURE_PROPLIST_DC; do
+		f_env_getdcpropertyvalue $DC $var
+		f_local_construct_addawkvar dc.$var "$C_ENV_XMLVALUE"
+
+		# handle unprefixed variables
+		if [[ ! " $S_CONFIGURE_PROPLIST_SERVER " =~ " $var " ]]; then
+			f_local_construct_addawkvar $var "$C_ENV_XMLVALUE"
+		fi
+	done
+
+	# add env-level props
+	for var in $S_CONFIGURE_PROPLIST_ENV; do
+		f_env_getenvpropertyvalue $var
+		f_local_construct_addawkvar env.$var "$C_ENV_XMLVALUE"
+
+		# handle unprefixed variables
+		if [[ ! " $S_CONFIGURE_PROPLIST_SERVER " =~ " $var " ]] && [[ ! " $S_CONFIGURE_PROPLIST_DC " =~ " $var " ]]; then
+			f_local_construct_addawkvar $var "$C_ENV_XMLVALUE"
+		fi
+	done
+
+	# add secret props
+	for var in $S_CONFIGURE_PROPLIST_SECRET; do
+		f_env_getsecretpropertyvalue $var
+		f_local_construct_addawkvar secret.$var "$C_ENV_XMLVALUE"
+
+		# handle unprefixed variables
+		if [[ ! " $S_CONFIGURE_PROPLIST_ENV " =~ " $var " ]] && [[ ! " $S_CONFIGURE_PROPLIST_SERVER " =~ " $var " ]] && [[ ! " $S_CONFIGURE_PROPLIST_DC " =~ " $var " ]]; then
+			f_local_construct_addawkvar $var "$C_ENV_XMLVALUE"
+		fi
+	done
+
+	echo "print" >> $S_CONFIGURE_AWKPROGRAM
+	echo "}" >> $S_CONFIGURE_AWKPROGRAM
+}
+
+function f_local_getproperties_secret() {
+	# read secret properties...
+	f_env_getsecretpropertylist
+	f_revertlist "$C_ENV_XMLVALUE"
+	S_CONFIGURE_PROPLIST_SECRET="$C_COMMON_LIST"
+}
+
+function f_local_getproperties_env() {
+	# read env properties...
+	f_env_getenvpropertylist
+	f_revertlist "$C_ENV_XMLVALUE"
+	S_CONFIGURE_PROPLIST_ENV="$C_COMMON_LIST"
+}
+
+function f_local_getproperties_dc() {
+	# echo read data center properties...
+	f_env_getdcpropertylist $DC
+	f_revertlist "$C_ENV_XMLVALUE"
+	S_CONFIGURE_PROPLIST_DC="$C_COMMON_LIST"
+}
+
+function f_local_getproperties_server() {
+	local P_SERVER=$1
+
+	# echo read server properties...
+	f_env_getserverpropertylist $DC $P_SERVER
+	f_revertlist "$C_ENV_XMLVALUE"
+	S_CONFIGURE_PROPLIST_SERVER="$C_COMMON_LIST"
+}
+
+function f_local_prepare_templates() {
+	local P_SERVER=$1
+	local P_COMPLIST="$2"
+
+	rm -rf $S_CONFIGURE_PREPAREDTEMPLATES
+	mkdir -p `dirname $S_CONFIGURE_PREPAREDTEMPLATES`
+	cp -R $P_DIR_TEMPLATES $S_CONFIGURE_PREPAREDTEMPLATES
+
+	local preparescript
+	local F_COMPDIR
+	for preparescript in `find $S_CONFIGURE_PREPAREDTEMPLATES -name "preconfigure.sh" | sort`; do
+		F_COMPDIR=`dirname $preparescript`
+		F_COMPNAME=`basename $F_COMPDIR`
+		
+		if [[ " $P_COMPLIST " =~ " $F_COMPNAME " ]]; then
+			local savedir=`pwd`
+			cd $F_COMPDIR
+
+			chmod 777 preconfigure.sh
+			echo "$P_SERVER: execute custom configuration script in $F_COMPDIR..."
+			. ./preconfigure.sh $C_ENV_ID $DC $P_SERVER
+
+			# remove script
+			rm -rf preconfigure.sh
+
+			cd $savedir
+		fi
+	done
+}
+
+function f_local_execute_server() {
+	local P_SERVER=$1
+
+	f_env_getserverconflist $DC $P_SERVER
+	local F_CONFCOMPLIST="$C_ENV_SERVER_CONFLIST"
+
+	if [ "$F_CONFCOMPLIST" = "" ]; then
+		if [ "$GETOPT_SHOWALL" = "yes" ]; then
+			echo server $P_SERVER: no configuration components defined. Skipped.
+		fi
+		return 1
+	fi
+	
+	# read server info
+	f_env_getxmlserverinfo $DC $P_SERVER
+	local F_NODELIST="$C_ENV_SERVER_HOSTLOGIN_LIST"
+
+	if [ "$GETOPT_SHOWALL" = "yes" ]; then
+		echo configure server $P_SERVER...
+	fi
+
+	# read server properties
+	f_local_getproperties_server $P_SERVER
+
+	# construct awk string to handle properties
+	f_local_construct_awk $P_SERVER
+
+	# prepare templates
+	f_local_prepare_templates $P_SERVER "$F_CONFCOMPLIST"
+
+	# generate files by templates
+	local nodehostlogin
+	for nodehostlogin in $F_NODELIST; do
+		f_local_generatenodefiles $P_SERVER $nodehostlogin "$F_CONFCOMPLIST"
+	done
+}
+
+function f_local_execute_dc() {
+	if [ "$GETOPT_SHOWALL" = "yes" ]; then
+		echo configure data center $DC...
+	fi
+
+	# read properties
+	f_local_getproperties_dc
+
+	# configure by server
+	f_env_getxmlserverlist $DC
+	local F_DC_SERVERLIST="$C_ENV_XMLVALUE"
+
+	if [ "$SRVNAME_LIST" = "" ]; then
+		SRVNAME_LIST="$F_DC_SERVERLIST"
+	fi
+
+	if [ "$GETOPT_SHOWALL" = "yes" ]; then
+		echo configure data center servers...
+	fi
+
+	f_checkvalidlist "$F_DC_SERVERLIST" "$SRVNAME_LIST"
+	local server
+	for server in $SRVNAME_LIST; do
+		f_local_execute_server $server
+	done
+}
+
+function f_local_execute_env() {
+	rm -rf $S_CONFIGURE_TMP
+	mkdir -p $S_CONFIGURE_TMP
+
+	# set environment
+	echo configure environment $C_ENV_ID in $P_DIR_LIVE using templates in $P_DIR_TEMPLATES ...
+
+	# read properties
+	f_local_getproperties_secret
+	f_local_getproperties_env
+
+	if [ "$GETOPT_SHOWALL" = "yes" ]; then
+		echo prepare templates in $S_CONFIGURE_PREPAREDTEMPLATES ...
+	fi
+
+	f_local_execute_dc
+
+	# delete tmp
+	rm -rf $S_CONFIGURE_TMP
+}
+
+# configure all
+f_local_execute_env
+
+echo configure.sh: finished.
