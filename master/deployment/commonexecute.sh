c6367216d6cda3321e5495a0761565d806c3c92c
vsavchik <vsavchik@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-06-16 17:27:44 +0000

    add iterator

--- master/deployment/commonexecute.sh
+++ master/deployment/commonexecute.sh
@@ -13,13 +13,12 @@ function f_common_execute_runcmd() {
 	local P_SUB=$1
 	local P_DC=$2
 	local P_FUNCTION=$3
-	local P_SERVERSET=$4
-	local P_SERVER_LIST="$5"
-	local P_NODE_LIST="$6"
-	local P_GROUP=$7
-	local P_SERVER=$8
-	local P_NODE=$9
-	local P_HOSTLOGIN=${10}
+	local P_SERVER_LIST="$4"
+	local P_NODE_LIST="$5"
+	local P_GROUP=$6
+	local P_SERVER=$7
+	local P_NODE=$8
+	local P_HOSTLOGIN=$9
 
 	if [ "$C_EXECUTE_CMD" = "" ]; then
 		echo C_EXECUTE_CMD is not set. Exiting
@@ -32,6 +31,13 @@ function f_common_execute_runcmd() {
 	elif [ "$P_SUB" = "getservers" ]; then
 		S_EXECUTE_SERVERS=$P_SERVER_LIST
 
+	elif [ "$P_SUB" = "startserver" ]; then
+		if [ "$C_ENV_SERVER_TYPE" = "generic.windows" ]; then
+			echo "ignore server=$P_EXECUTE_SRVNAME, type=$C_ENV_SERVER_TYPE (windows)"
+			S_EXECUTE_ENABLED=no
+			return 1
+		fi
+
 	elif [ "$P_SUB" = "executenode" ]; then
 		./oneruncmd.sh $P_SERVER $P_NODE $P_HOSTLOGIN "$C_EXECUTE_CMD"
 	fi
@@ -41,26 +47,6 @@ function f_common_execute_function() {
 	local P_SUB=$1
 	local P_DC=$2
 	local P_FUNCTION=$3
-	local P_SERVERSET=$4
-	local P_SERVER_LIST="$5"
-	local P_NODE_LIST="$6"
-	local P_GROUP=$7
-	local P_SERVER=$8
-	local P_NODE=$9
-	local P_HOSTLOGIN=${10}
-
-	case "$P_FUNCTION" in
-# build operations
-		RUNCMD)
-			f_common_execute_runcmd $P_SUB $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $P_NODE $P_HOSTLOGIN
-			;;
-	esac
-}
-
-function f_common_execute_node() {
-	local P_DC=$1
-	local P_FUNCTION=$2
-	local P_SERVERSET=$3
 	local P_SERVER_LIST="$4"
 	local P_NODE_LIST="$5"
 	local P_GROUP=$6
@@ -68,17 +54,34 @@ function f_common_execute_node() {
 	local P_NODE=$8
 	local P_HOSTLOGIN=$9
 
-	f_common_execute_function "executenode" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $P_NODE $P_HOSTLOGIN
+	case "$P_FUNCTION" in
+# build operations
+		RUNCMD)
+			f_common_execute_runcmd $P_SUB $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $P_NODE $P_HOSTLOGIN
+			;;
+	esac
+}
+
+function f_common_execute_node() {
+	local P_DC=$1
+	local P_FUNCTION=$2
+	local P_SERVER_LIST="$3"
+	local P_NODE_LIST="$4"
+	local P_GROUP=$5
+	local P_SERVER=$6
+	local P_NODE=$7
+	local P_HOSTLOGIN=$8
+
+	f_common_execute_function "executenode" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $P_NODE $P_HOSTLOGIN
 }
 
 function f_common_execute_server() {
 	local P_DC=$1
 	local P_FUNCTION=$2
-	local P_SERVERSET=$3
-	local P_SERVER_LIST="$4"
-	local P_NODE_LIST="$5"
-	local P_GROUP=$6
-	local P_SERVER=$7
+	local P_SERVER_LIST="$3"
+	local P_NODE_LIST="$4"
+	local P_GROUP=$5
+	local P_SERVER=$6
 
 	f_env_getxmlserverinfo $P_DC $P_SERVER $GETOPT_DEPLOYGROUP
 	local F_LOCAL_HOSTLOGINLIST="$C_ENV_SERVER_HOSTLOGIN_LIST"
@@ -92,7 +95,7 @@ function f_common_execute_server() {
 	fi
 
 	S_EXECUTE_ENABLED=yes
-	f_execute_function "startserver" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER
+	f_execute_function "startserver" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER
 	if [ "$S_EXECUTE_ENABLED" != "yes" ]; then
 		if [ "$GETOPT_SHOWALL" = "yes" ]; then
 			echo "ignore server=$P_SERVER, type=$C_ENV_SERVER_TYPE (filtered)"
@@ -106,34 +109,33 @@ function f_common_execute_server() {
 	local hostlogin
 	for hostlogin in $C_ENV_SERVER_HOSTLOGIN_LIST; do
 		if [ "$P_NODE_LIST" = "" ] || [[ " $P_NODE_LIST " =~ " $NODE " ]]; then
-			f_common_execute_node $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $NODE $hostlogin
+			f_common_execute_node $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER $NODE $hostlogin
 		fi
 		NODE=$(expr $NODE + 1)
 	done
 
-	f_execute_function "finishserver" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER
+	f_execute_function "finishserver" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $P_SERVER
 }
 
 function f_common_execute_group() {
 	local P_DC=$1
 	local P_FUNCTION=$2
-	local P_SERVERSET=$3
-	local P_SERVER_LIST="$4"
-	local P_NODE_LIST="$5"
-	local P_GROUP=$6
+	local P_SERVER_LIST="$3"
+	local P_NODE_LIST="$4"
+	local P_GROUP=$5
 
 	# limit server and order
 	S_EXECUTE_SERVERS=
-	f_execute_function "getservers" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP
+	f_execute_function "getservers" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP
 
 	# iterate servers	
 	local server
 	for server in $S_EXECUTE_SERVERS; do
 		# execute server
-		f_common_execute_server $P_DC $P_FUNCTION $P_SERVERSET "$F_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $server
+		f_common_execute_server $P_DC $P_FUNCTION "$F_SERVER_LIST" "$P_NODE_LIST" $P_GROUP $server
 	done
 
-	f_execute_function "finishgroup" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP
+	f_execute_function "finishgroup" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$P_NODE_LIST" $P_GROUP
 }
 
 function f_common_execute_splitservers() {
@@ -149,10 +151,9 @@ function f_common_execute_splitservers() {
 }
 
 function f_common_execute_set() {
-	local P_DC=$1
-	local P_FUNCTION=$2
-	local P_SERVERSET=$3
-	local P_EXECUTE_LIST="$4"
+	local P_FUNCTION=$1
+	local P_DC=$2
+	local P_EXECUTE_LIST="$3"
 
 	# split into servers and nodes
 	S_EXECUTE_PARAM_SERVERS=
@@ -172,14 +173,14 @@ function f_common_execute_set() {
 	S_EXECUTE_ENABLED=
 
 	# split into groups
-	f_execute_function "getgroups" $P_DC $P_FUNCTION $P_SERVERSET "$F_SERVER_LIST" "$S_EXECUTE_PARAM_NODES"
+	f_execute_function "getgroups" $P_DC $P_FUNCTION "$F_SERVER_LIST" "$S_EXECUTE_PARAM_NODES"
 
 	# iterate groups
 	local group
 	for group in $S_EXECUTE_GROUPS; do
 		# execute group
-		f_common_execute_group $P_DC $P_FUNCTION $P_SERVERSET "$F_SERVER_LIST" "$S_EXECUTE_PARAM_NODES" $group 
+		f_common_execute_group $P_DC $P_FUNCTION "$F_SERVER_LIST" "$S_EXECUTE_PARAM_NODES" $group 
 	done
 
-	f_execute_function "finishdc" $P_DC $P_FUNCTION $P_SERVERSET "$P_SERVER_LIST" "$S_EXECUTE_PARAM_NODES"
+	f_execute_function "finishdc" $P_DC $P_FUNCTION "$P_SERVER_LIST" "$S_EXECUTE_PARAM_NODES"
 }
--- master/deployment/runcmd.sh
+++ master/deployment/runcmd.sh
@@ -22,86 +22,18 @@ SRVNAME_LIST=$1
 
 # load common functions
 . ./common.sh
-. ./commondeploy.sh
+. ./commonexecute.sh
 
 # execute
-function f_local_execute_node() {
-	local P_EXECUTE_SRVNAME=$1
-	local P_NODE=$2
-	local P_EXECUTE_HOSTLOGIN=$3
 
-	if [ "$GETOPT_EXECUTE" = "yes" ]; then
-		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD"
-
-		local F_LOGCMD="echo `date` \"(SSH_CLIENT=$SSH_CLIENT): $P_RUNCMD_CMD\" >> ~/execute.log"
-		if [ "$GETOPT_SKIPERRORS" = "yes" ]; then
-			if [ "$C_ENV_PROPERTY_KEYNAME" != "" ]; then
-				ssh -i $C_ENV_PROPERTY_KEYNAME -n $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-			else
-				ssh -n $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-			fi
-		else
-			f_run_cmdcheck $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-		fi
-
-		if [ "$C_ENV_PROPERTY_KEYNAME" != "" ]; then
-			ssh -i $C_ENV_PROPERTY_KEYNAME -n $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
-		else
-			ssh -n $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
-		fi
-	else
-		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD (showonly)"
-	fi
-}
-
-function f_local_execute_server() {
-	local P_EXECUTE_SRVNAME=$1
-
-	f_env_getxmlserverinfo $DC $P_EXECUTE_SRVNAME $GETOPT_DEPLOYGROUP
-	local F_LOCAL_HOSTLOGINLIST="$C_ENV_SERVER_HOSTLOGIN_LIST"
-
-	# check affected
-	if [ "$F_LOCAL_HOSTLOGINLIST" = "" ]; then
-		echo "ignore server=$P_EXECUTE_SRVNAME, type=$C_ENV_SERVER_TYPE (no hosts)"
-		return 1
-	fi
-	if [ "$C_ENV_SERVER_TYPE" = "generic.windows" ]; then
-		echo "ignore server=$P_EXECUTE_SRVNAME, type=$C_ENV_SERVER_TYPE (windows)"
-		return 1
-	fi
-
-	echo ============================================ execute server=$P_EXECUTE_SRVNAME, type=$C_ENV_SERVER_TYPE...
-
-	local NODE=1
-	local hostlogin
-	for hostlogin in $C_ENV_SERVER_HOSTLOGIN_LIST; do
-		if [ "$EXECUTE_NODELIST" = "" ] || [[ " $EXECUTE_NODELIST " =~ " $NODE " ]]; then
-			f_local_execute_node $P_EXECUTE_SRVNAME $NODE $hostlogin
-		fi
-		NODE=$(expr $NODE + 1)
-	done
-}
-
-# get server list
-function f_local_executedc() {
+function f_local_executeall() {
 	echo execute datacenter=$DC...
-	f_env_getxmlserverlist $DC
-	local F_SERVER_LIST=$C_ENV_XMLVALUE
 
-	f_checkvalidlist "$F_SERVER_LIST" "$SRVNAME_LIST"
-	f_getsubset "$F_SERVER_LIST" "$SRVNAME_LIST"
-	F_SERVER_LIST=$C_COMMON_SUBSET
-
-	# iterate servers
-	local server
-	for server in $F_SERVER_LIST; do
-		f_local_execute_server $server
-	done
+	export C_EXECUTE_CMD="$P_RUNCMD_CMD"
+	f_common_execute_set "runcmd" $DC $SRVNAME_LIST
 }
 
 # execute in environment (except for windows-based)
-
-# execute datacenter
-f_local_executedc
+f_local_executeall
 
 echo runcmd.sh: SUCCESSFULLY DONE.
