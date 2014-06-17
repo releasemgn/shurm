ebcdaac2fe7d71d2cbec4ebbbc503697959fbd57
vsavchik <vsavchik@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-06-17 17:28:41 +0000

    add newkey

--- master/deployment/getopts.sh
+++ master/deployment/getopts.sh
@@ -151,6 +151,10 @@ while [ "$#" -gt "0" ] && [[ "$1" =~ ^-[a-z] ]]; do
 			export GETOPT_KEY=$2
 			shift 2
 			;;
+		'-newkey')
+			export GETOPT_NEWKEY=$2
+			shift 2
+			;;
 		*)
 			echo getopts.sh: invalid option=$1 in command line. Exiting
 			exit 1
