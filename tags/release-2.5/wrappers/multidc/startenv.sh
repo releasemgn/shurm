f8ec1fd3d912a28e8d29be9b778c76359e469262
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-07-16 13:20:12 +0000

    release 2.5

---
+++ tags/release-2.5/wrappers/multidc/startenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd `dirname $0`
+RUNPATH=`pwd`
+
+# set env/dc context
+. ./_context.sh
+
+cd ../..
+. ./getopts.sh
+. ./setenv.sh $C_CONTEXT_ENV
+
+echo "`date`: execute ./startenv.sh -dc $C_CONTEXT_DC $*" >> $RUNPATH/deploy.log
+./startenv.sh -dc $C_CONTEXT_DC $* | tee -a $RUNPATH/deploy.log; F_STATUS=${PIPESTATUS[0]}
+
+exit $F_STATUS
+
