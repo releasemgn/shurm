04a63b09a41204fc1273b96c9e02ea04803d60f5
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-07-16 11:23:59 +0000

    release 2.5

---
+++ tags/release-2.5/deployment/oneruncmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2011-2014 vsavchik@gmail.com
+
+P_EXECUTE_SRVNAME=$1
+P_NODE=$2
+P_EXECUTE_HOSTLOGIN=$3
+P_RUNCMD_CMD="$4"
+
+. ./common.sh
+
+function f_local_execute() {
+	if [ "$GETOPT_EXECUTE" = "yes" ]; then
+		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD"
+
+		local F_LOGCMD="echo `date` \"(SSH_CLIENT=$SSH_CLIENT): $P_RUNCMD_CMD\" >> ~/execute.log"
+		f_run_cmdcheck $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
+		f_run_cmd $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
+	else
+		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD (showonly)"
+	fi
+}
+
+f_local_execute
