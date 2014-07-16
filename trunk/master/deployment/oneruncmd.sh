aafbd14f64d67bffd7a1dbecf700133a0e68620a
vsavchik <vsavchik@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-07-16 11:17:43 +0000

    remove unneeded

--- trunk/master/deployment/oneruncmd.sh
+++ trunk/master/deployment/oneruncmd.sh
@@ -12,27 +12,9 @@ function f_local_execute() {
 	if [ "$GETOPT_EXECUTE" = "yes" ]; then
 		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD"
 
-		local F_KEY=$C_ENV_PROPERTY_KEYNAME
-		if [ "$GETOPT_KEY" != "" ]; then
-			F_KEY=$GETOPT_KEY
-		fi
-
 		local F_LOGCMD="echo `date` \"(SSH_CLIENT=$SSH_CLIENT): $P_RUNCMD_CMD\" >> ~/execute.log"
-		if [ "$GETOPT_SKIPERRORS" = "yes" ]; then
-			if [ "$F_KEY" != "" ]; then
-				ssh -i $F_KEY -n $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-			else
-				ssh -n $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-			fi
-		else
-			f_run_cmdcheck $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
-		fi
-
-		if [ "$F_KEY" != "" ]; then
-			ssh -i $F_KEY -n $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
-		else
-			ssh -n $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
-		fi
+		f_run_cmdcheck $P_EXECUTE_HOSTLOGIN "$F_LOGCMD"
+		f_run_cmdcheck $P_EXECUTE_HOSTLOGIN "$P_RUNCMD_CMD"
 	else
 		echo "$P_EXECUTE_HOSTLOGIN: $P_RUNCMD_CMD (showonly)"
 	fi
