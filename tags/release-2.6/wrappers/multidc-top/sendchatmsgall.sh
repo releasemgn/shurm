5707a39d42063ab9e0ea326f3366a7ff4c937812
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-08-05 15:52:15 +0000

    release 2.6

---
+++ tags/release-2.6/wrappers/multidc-top/sendchatmsgall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd `dirname $0`
+RUNPATH=`pwd`
+
+cd ..
+. ./getopts.sh
+. ./setenv.sh `basename $RUNPATH`.xml
+. ./common.sh
+
+P_EXECUTE_MSG="$1"
+shift 1
+
+echo "`date`: execute ./sendchatmsg.sh -dc all "$P_EXECUTE_MSG" $*" >> $RUNPATH/deploy.log
+./sendchatmsg.sh -dc all "$P_EXECUTE_MSG" $* | tee -a $RUNPATH/deploy.log; F_STATUS=${PIPESTATUS[0]}
+
+exit $F_STATUS
+
