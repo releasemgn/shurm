ff036cf50a8a088c89b81dfa84a226f2987a27f3
vsavchik@gmail.com <vsavchik@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-09-05 15:11:39 +0000

    add express

--- trunk/master/common/common.sh
+++ trunk/master/common/common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2011-2014 vsavchik@gmail.com
 
-S_COMMON_EXTLIST="sh xml txt properties conf config xconf groovy"
+S_COMMON_EXTLIST="sh xml txt properties conf config xconf groovy sql"
 S_COMMON_FINDEXTLIST='-name "*.sh" -o -name "*.xml" -o -name "*.txt" -o -name "*.properties" -o -name "*.conf" -o -name "*.config" -o -name "*.xconf" -o -name "*.sql"'
 
 function f_dos2unix_file() {
