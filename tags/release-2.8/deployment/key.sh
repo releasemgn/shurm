64737238c63aac176b982ce6fe5f05c41a6c1597
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-09-09 15:10:56 +0000

    release 2.8

---
+++ tags/release-2.8/deployment/key.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Copyright 2011-2014 vsavchik@gmail.com
+
+. ./getopts.sh
+
+# check params
+# should be specific DC
+DC=$GETOPT_DC
+if [ "$DC" = "" ]; then
+	echo key.sh: DC not set
+	exit 1
+fi
+
+P_RUNCMD_CMD="$1"
+if [ "$P_RUNCMD_CMD" = "" ]; then
+	echo key.sh: P_RUNCMD_CMD not set
+	exit 1
+fi
+shift 1
+
+SRVNAME_LIST=$*
+
+# load common functions
+. ./common.sh
+. ./commonexecute.sh
+
+# execute
+
+function f_local_executeall() {
+	export C_EXECUTE_CMD=$P_RUNCMD_CMD
+	f_common_execute_unique "KEY" $DC "$SRVNAME_LIST"
+}
+
+# execute in environment (except for windows-based)
+f_local_executeall
+
+echo key.sh: SUCCESSFULLY DONE.
