54c8ef113c584d4d10353b26e032210eca40eedc
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-09-25 15:27:32 +0000

    release 2.9

---
+++ tags/release-2.9/wrappers/multidc/database/manage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd `dirname $0`
+RUNPATH=`pwd`
+
+# set env/dc context
+. ../_context.sh
+
+cd ../../../../database
+. ./getopts.sh
+. ./setenv.sh $C_CONTEXT_ENV
+
+./manage.sh -dc $C_CONTEXT_DC $*
