2ce2f04fc718320327afb1c254621861f67e353c
vsavchik@gmail.com <vsavchik@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2014-11-20 15:40:59 +0000

    add hosts

--- trunk/master/wrappers/multidc/hosts.sh
+++ trunk/master/wrappers/multidc/hosts.sh
@@ -10,8 +10,8 @@ cd ../..
 . ./getopts.sh
 . ./setenv.sh $C_CONTEXT_ENV
 
-echo "`date`: execute ./runcmd.sh -dc $C_CONTEXT_DC $*" >> $RUNPATH/deploy.log
-./runcmd.sh -dc $C_CONTEXT_DC $* | tee -a $RUNPATH/deploy.log; F_STATUS=${PIPESTATUS[0]}
+echo "`date`: execute ./hosts.sh -dc $C_CONTEXT_DC $*" >> $RUNPATH/deploy.log
+./hosts.sh -dc $C_CONTEXT_DC $* | tee -a $RUNPATH/deploy.log; F_STATUS=${PIPESTATUS[0]}
 
 exit $F_STATUS
 
