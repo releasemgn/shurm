9d8ed07227fb3004383e6d2241dc17bc1ac9f392
releasemgn@gmail.com <releasemgn@gmail.com@b1c4bfcf-c8a0-67d5-5b06-8b8b53859406>
2015-03-12 13:42:26 +0000

    release 4.0

---
+++ tags/release-4.0/makedistr/patch-checkout.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+# Copyright 2011-2013 vsavchik@gmail.com
+
+cd `dirname $0`
+. ./getopts.sh
+
+P_PATCHPATH=$1
+P_MODULESET=$2
+P_MODULENAME=$3
+P_REPOSITORY=$4
+P_MODULEPATH=$5
+P_TAG=$6
+
+# check params
+if [ "$P_PATCHPATH" = "" ]; then
+	echo patch-checkout.sh: P_PATCHPATH not set
+	exit 1
+fi
+if [ "$P_MODULESET" = "" ]; then
+	echo patch-checkout.sh: P_MODULESET not set
+	exit 1
+fi
+if [ "$P_MODULENAME" = "" ]; then
+	echo patch-checkout.sh: P_MODULENAME not set
+	exit 1
+fi
+if [ "$P_REPOSITORY" = "" ]; then
+	echo patch-checkout.sh: P_REPOSITORY not set
+	exit 1
+fi
+if [ "$P_MODULEPATH" = "" ]; then
+	echo patch-checkout.sh: P_MODULEPATH not set
+	exit 1
+fi
+if [ "$P_TAG" = "" ]; then
+	echo patch-checkout.sh: P_TAG not set
+	exit 1
+fi
+
+function f_execute_all() {
+	# checkout
+	./vcscheckout.sh $P_PATCHPATH $P_MODULENAME "$P_REPOSITORY" "$P_MODULEPATH" ignore $P_TAG
+	if [ $? -ne 0 ]; then
+        	echo "patch-checkout.sh: having problem to check out"
+	        exit 1
+	fi
+}
+
+f_execute_all
+
+echo patch-checkout.sh: finished.
